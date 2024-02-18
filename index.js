54c7d7000dc3f98a30fd8f86392040b17e221179
Kogut <marcelokogut3@gmail.com>
2024-02-18 04:28:04 +0200

    adding express

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require("express");
+
 require("dotenv").config();
 
 require("./commands/slashCommands.js");
 require("./commands/messageCommands.js");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen();
